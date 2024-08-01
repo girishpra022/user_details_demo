6db0771a3cb6c3657a4d6759564a8732b37e9d1b
girishpra022 <girishpractise022@gmail.com>
2024-08-01 17:22:44 +0530

    modified connect string

--- server/src/config/db.config.ts
+++ server/src/config/db.config.ts
@@ -3,15 +3,12 @@ import mongoose from "mongoose";
 
 dotenv.config();
 
-
 const username = process.env.monogo_username;
 const password = process.env.password;
 const dbName = process.env.dbName;
+const mongoDbCluster=process.env.MONGODB_CLUSTER;
 
-console.log('username', username)
-
-
-const connectionString = `mongodb+srv://${username}:${password}@usercluster.jkb8ust.mongodb.net/userDB?retryWrites=true&w=majority&appName=userCluster`;
+const connectionString = `mongodb+srv://${username}:${password}@${mongoDbCluster}/userDB?retryWrites=true&w=majority&appName=userCluster`;
 
 const options = {
   autoIndex: false, 
@@ -20,7 +17,6 @@ const options = {
   socketTimeoutMS: 45000, 
   family: 4, 
 };
-
 
 export const db = mongoose
   .connect(connectionString, options)
